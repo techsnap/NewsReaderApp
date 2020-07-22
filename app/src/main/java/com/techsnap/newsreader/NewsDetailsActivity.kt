e7a45bb6b2fdc59cb7b4d397cf354b9cd4b82943
techsnap <dushyantsharma17117@gmail.com>
2020-07-22 18:02:01 +0530

    Added progress bar

--- app/src/main/java/com/techsnap/newsreader/NewsDetailsActivity.kt
+++ app/src/main/java/com/techsnap/newsreader/NewsDetailsActivity.kt
@@ -2,13 +2,16 @@ package com.techsnap.newsreader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.RelativeLayout
 
 class NewsDetailsActivity : AppCompatActivity() {
 
     lateinit var webView: WebView
+    lateinit var progressBarLayout: RelativeLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +20,15 @@ class NewsDetailsActivity : AppCompatActivity() {
         val url = intent.getStringExtra("url")
 
         webView = findViewById(R.id.webView)
-        webView.webViewClient = MyWebViewClient()
+        progressBarLayout = findViewById(R.id.progressBarLayout)
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                progressBarLayout.visibility = View.GONE
+            }
+        }
+
         webView.loadUrl(url)
 
         webView.settings.javaScriptEnabled = true
@@ -31,10 +42,4 @@ class NewsDetailsActivity : AppCompatActivity() {
         }
     }
 
-    class MyWebViewClient: WebViewClient() {
-        override fun onPageFinished(view: WebView?, url: String?) {
-            super.onPageFinished(view, url)
-
-        }
-    }
 }
